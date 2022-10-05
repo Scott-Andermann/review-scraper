dc8291198cac5c47436e1bb924523cfee5d8ca53
Scott A <scottandermann@gmail.com>
2022-10-05 12:21:26 -0400

    update graphs

--- scraper-app/src/components/Graph/Graph.js
+++ scraper-app/src/components/Graph/Graph.js
@@ -6,7 +6,38 @@ import './Graph.css';
 const Graph = ({ data }) => {
     // acting funny since these are not state variables
 
-    const [traces, setTraces] = useState([]);
+    const [sent, setSent] = useState(false);
+    const [avgSent, setAvgSent] = useState(false);
+    const [rating, setRating] = useState(true);
+    const [avgRating, setAvgRating] = useState(false);
+
+    const onClickPlot = (e) => {
+        console.log(e.target.name);
+        if (e.target.name === 'sentiment') {
+            setAvgSent(false)
+            setRating(false)
+            setAvgRating(false)
+            setSent(true)
+        }
+        if (e.target.name === 'avgSentiment') {
+            setAvgSent(true)
+            setRating(false)
+            setAvgRating(false)
+            setSent(false)
+        }
+        if (e.target.name === 'rating') {
+            setAvgSent(false)
+            setRating(true)
+            setAvgRating(false)
+            setSent(false)
+        }
+        if (e.target.name === 'avgRating') {
+            setAvgSent(false)
+            setRating(false)
+            setAvgRating(true)
+            setSent(false)
+        }
+    }
 
     let sentimentTraces = []
     let avgSentimentTraces = []
@@ -61,30 +92,39 @@ const Graph = ({ data }) => {
 
     return (
         <div className='graph-wrapper'>
-            <button>Sentiment</button>
-            <button>Average Sentiment</button>
-            <button>Rating</button>
-            <button>Average Rating</button>
+            <button name='sentiment' onClick={onClickPlot}>Sentiment</button>
+            <button name='avgSentiment' onClick={onClickPlot}>Average Sentiment</button>
+            <button name='rating' onClick={onClickPlot}>Rating</button>
+            <button name='avgRating' onClick={onClickPlot}>Average Rating</button>
+            {sent &&
+                <Plot
+                    className='one'
+                    data={sentimentTraces}
+                    layout={{ width: 600, height: 400, title: 'A fancy Sentiment plot', yaxis: {range: [-1, 1]} }}
+                />
+            }
+            {rating &&
+
             <Plot
-                className='one'
-                data={sentimentTraces}
-                layout={{ width: 600, height: 400, title: 'A fancy Sentiment plot', yaxis: {range: [-1, 1]} }}
-            />
-            {/* <Plot
             className='two'
-                data={starTraces}
-                layout={{ width: 600, height: 400, title: 'A fancy Star Rating plot', yaxis: {range: [0.25, 5.25]} }}
+            data={starTraces}
+            layout={{ width: 600, height: 400, title: 'A fancy Star Rating plot', yaxis: {range: [0.25, 5.25]} }}
             />
+            }
+            {avgSent &&
             <Plot
             className='three'
-                data={avgSentimentTraces}
-                layout={{ width: 600, height: 400, title: 'Average Sentiment Plot', yaxis: {range: [-1, 1]} }}
+            data={avgSentimentTraces}
+            layout={{ width: 600, height: 400, title: 'Average Sentiment Plot', yaxis: {range: [-1, 1]} }}
             />
+            }
+            {avgRating &&
             <Plot
             className='four'
-                data={avgStarTraces}
-                layout={{ width: 600, height: 400, title: 'Average Star Rating plot', yaxis: {range: [0.25, 5.25]}}}
-            /> */}
+            data={avgStarTraces}
+            layout={{ width: 600, height: 400, title: 'Average Star Rating plot', yaxis: {range: [0.25, 5.25]}}}
+            />
+            }
         </div>
     );
 }
